ec824ebc8c2887303ab88527f7511afc1efdf8e8
FarrylC <changfa@sheridancollege.ca>
2022-01-16 18:19:43 -0500

    Create Game Screen

---
+++ Game/Under Choices/Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public float startingFund, timerLength;
+
+    float playerFunds, timer;
+    int currentNight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
