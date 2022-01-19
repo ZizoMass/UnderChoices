474cd484aa92f15eb90c8799148265d6620ee9b3
FarrylC <changfa@sheridancollege.ca>
2022-01-19 16:42:53 -0500

    Implement CSV Data Parsing
    Implement scriptable objects for media posts and importing media post data from CSV file.

---
+++ Game/Under Choices/Assets/Editor/MediaPost.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Media Post", menuName = "Assets/New Media Post")]
+public class MediaPost : ScriptableObject
+{
+    public enum Subject { Government, Violence, Health, Radicalism }
+    public enum Reaction { Happy, Sad, Angry }
+
+    public int postNumber, day, baseEngagement, boostedEngagement;
+    public string publisher, headline, imageFilePath;
+    public Subject subject;
+    public Reaction reaction;
+    public List<string> hashtags;
+    public float boostCost;
+}
--- Game/Under Choices/Assets/Scripts/GameController.cs
+++ Game/Under Choices/Assets/Scripts/GameController.cs
@@ -36,6 +36,7 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        print(Application.dataPath);
         // Check which scene to load
         CheckScene();
     }
