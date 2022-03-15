b2d9b5f9cd61f04d0b99a4b46b86f0865e149331
Wilson Chan <chanwils@sheridancollege.ca>
2022-03-14 21:38:32 -0400

    Added coroutines that disable the particle effects after a public duration variable on the event_animation_manager script.

--- Game/Under Choices/Assets/Scripts/event_animation_manager.cs
+++ Game/Under Choices/Assets/Scripts/event_animation_manager.cs
@@ -13,6 +13,7 @@ public class event_animation_manager : MonoBehaviour
     private double videoCurrentTime;
     private double videoTotalTime;
     private bool isVideoPlaying;
+    public float riotDuration;
 
 
     // Start is called before the first frame update
@@ -20,6 +21,7 @@ public class event_animation_manager : MonoBehaviour
     {
         eventType = Event.none;
         childEventsArray = new Transform[5];
+        riotDuration = 5f;
         EventsSetup();
 
     }
@@ -28,7 +30,9 @@ public class event_animation_manager : MonoBehaviour
     void Update()
     {
         deleteLaterTestingEventTrigger();
-        HandleEventVideoPlaying();
+        EventHandleVideoPlaying();
+        print("event:" + eventType);
+        
     }
 
     /*  Event eventSelection (Event eventName)
@@ -53,7 +57,6 @@ public class event_animation_manager : MonoBehaviour
         {
             childEventsArray[i] = transform.GetChild(i);
         }
-        Debug.Log(childEventsArray.Length);
 
     }
     public void EventActivate(Event desiredEvent)
@@ -75,19 +78,25 @@ public class event_animation_manager : MonoBehaviour
                 isVideoPlaying = true;
                 videoTotalTime = childEventsArray[desiredEventIndex].gameObject.GetComponent<VideoPlayer>().clip.length;//checks if there's a vido clip associated with it to get the current frame so I know when the clip ends
             }
+            else if (childEventsArray[i] == childEventsArray[desiredEventIndex] && eventType == Event.riot)//If its the Riot - the coroutine will run for a duration before turning itself off (the "riotDuration" float)
+            {
+               childEventsArray[i].gameObject.SetActive(true);
+               StartCoroutine(EventPrimaryRiotParticleOff(riotDuration));
+            }
             else if (childEventsArray[i] == childEventsArray[desiredEventIndex] && (eventType != Event.fired || eventType != Event.hijacked || eventType != Event.speech || eventType != Event.sirens))
             {
                 childEventsArray[i].gameObject.SetActive(true);
             }
+         
             else
             {
                 childEventsArray[i].gameObject.SetActive(false);
             }
         }
     }
-    void HandleEventVideoPlaying()//Checks per frame for when the video clip is over so it can turn itself off.
+    void EventHandleVideoPlaying()//Checks per frame for when the video clip is over so it can turn itself off.
     {
-        if (isVideoPlaying)
+        if (isVideoPlaying && eventType != Event.none)
         {
             int videoEventIndex = (int)eventType; // can use this as an index to manipulate the appropriate index
             videoCurrentTime = childEventsArray[videoEventIndex].gameObject.GetComponent<VideoPlayer>().time; //IF THERE IS AN ERROR: it likely happened when the enum eventType swaps from
@@ -96,10 +105,30 @@ public class event_animation_manager : MonoBehaviour
             {
                 childEventsArray[videoEventIndex].gameObject.SetActive(false);
                 isVideoPlaying = false;
-                eventType = Event.none;
+                eventType = Event.none;//This is critical - by it setting itself back to none, the game objects turn off and allows for selection of others
             }
         }
     }
+    public IEnumerator EventPrimaryRiotParticleOff(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        int eventRiotIndex = (int)Event.riot;
+        ParticleSystem[] childrenParticleSytems = childEventsArray[eventRiotIndex].gameObject.GetComponentsInChildren<ParticleSystem>();
+        foreach (ParticleSystem childParticle in childrenParticleSytems)
+        {
+            childParticle.Stop();
+        }
+        StartCoroutine(EventRiotParticleDisable());
+    }
+    private IEnumerator EventRiotParticleDisable()
+    {
+        yield return new WaitForSeconds(2.5f);
+        int eventRiotIndex = (int)Event.riot;
+        eventType = Event.none; // A bug persists where if you set it to Riot, then after Riot ends and game object is disabled, if you try to play another animation event, it bugs and doesn't auto turn off the animation. 
+        //*continued* BUT if you wait ~>10 seconds after the Riot event ends, then decide to swap back to i.e. Speech, it will play with no issue. The bug behaviour is that the enum swaps back to None, the frame after selecting another event
+        childEventsArray[eventRiotIndex].gameObject.SetActive(false);
+    }
+
     void deleteLaterTestingEventTrigger()
     {
         if(eventType == Event.speech)
