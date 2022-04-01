d0d481e529533a2f27773d9b61f89449005e7e3b
Tiago Massochin <zizomass@outlook.com>
2022-04-01 03:31:50 -0400

    UC-782 feat: new scene for the violence ending
    The audio files, project files, and video files referents to the violence ending ware added to the repository.
    
    A new scene for the violence ending was created.
    
    When started, the scene auto play the properly animation and audio for the violence ending.
    
    After the video is finished, the scene automatically goes to the Main Title scene.

---
+++ Game/Under Choices/Assets/Scripts/End_Controller.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+
+public class End_Controller : MonoBehaviour
+{
+    public VideoClip myClip;
+    VideoPlayer videoPlayer;
+
+    //Reference to the Fmod files
+    FMOD.Studio.EventInstance instance;
+    FMOD.Studio.Bus MasterBus;
+
+    //Scene to be loaded after the video finish
+    public string SceneName;
+
+    bool waitForFirstFrame;
+    bool canSetSkipOnDrop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.runInBackground = true;
+        videoPlayer = gameObject.AddComponent<VideoPlayer>();
+
+        //Check if the video is finished
+        videoPlayer.loopPointReached += LoadScene;
+
+        //Define the reference with the Master Bus from Fmod
+        MasterBus = FMODUnity.RuntimeManager.GetBus("Bus:/");
+
+        //Stop all Fmod audio events before start the scene
+        MasterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+
+        waitForFirstFrame = true;
+        canSetSkipOnDrop = false;
+
+        instance = FMODUnity.RuntimeManager.CreateInstance("event:/Video Sound/Violence End");
+
+        LoadAndPlay();
+
+    }
+
+    public void LoadAndPlay()
+    {
+        //Start the video clip
+        videoPlayer.clip = myClip;
+        videoPlayer.Play();
+        //play video sound;
+        instance.start();
+    }
+
+    //If the video is finished, load a new scene
+    void LoadScene(VideoPlayer vp)
+    {
+        //Stop all Fmod audio events before start the scene
+        MasterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        //load next scene
+        SceneManager.LoadScene(SceneName);
+    }
+
+}
