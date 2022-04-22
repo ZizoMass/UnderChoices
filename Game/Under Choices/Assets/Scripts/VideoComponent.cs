60a9882d7bc7656b125ea7da11c9dde57cb7ba51
Tiago Massochin <zizomass@outlook.com>
2022-04-21 21:49:30 -0400

    UC-882 index: video files moved to the StreamingAssets folder

---
+++ Game/Under Choices/Assets/Scripts/VideoComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using System.IO;
+
+
+
+public class VideoComponent : MonoBehaviour
+{
+    public UnityEngine.Video.VideoPlayer videoPlayer;
+    private string status;
+    public string videoName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, videoName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "myFile.mp4");
+    }
+}
